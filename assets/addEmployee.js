1786166ae1c0a329bc3ff957fb1393918cafa06e
Craig Gant <craigkgant@gmail.com>
2020-01-13 21:17:46 -0500

    added function addEmployee

--- assets/addEmployee.js
+++ assets/addEmployee.js
@@ -2,8 +2,25 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
-function viewEmployee() {
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type:"input",
+      name:"firstName",
+      message:"What's the employee's first name?"
+    },
+    {
+      type:"input",
+      name:"lastName",
+      message:"What's the employee's last name?"
+    }
+  ]).then(res=>{
+    let query = "INSERT INTO employee (first_name, last_name) VALUES ?"
+    connection.query(query + [res.firstName, res.lastName]);
+    console.log(`Success! ${res.firstName} ${res.lastName} has been created.`);
+    console.log("-------------------");
+  })
 
 }
 
-module.exports = viewEmployee;
+module.exports = addEmployee;
--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,13 @@
-USE 
+USE employeesDB;
 
-insert into deparment
+INSERT INTO department (name)
+VALUES ("IT");
+
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Ben", "Solo");
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Rey", "Palpatine");
