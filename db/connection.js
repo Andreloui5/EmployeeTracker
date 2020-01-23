c251fb7d463a1b2ba97a1cf2c83f35970f2d1575
Craig Gant <craigkgant@gmail.com>
2020-01-23 09:34:59 -0500

    removed connection.js from gitignore

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+
+  host:"localhost",
+  route:3060,
+  user:"",
+  password:"",
+  database:"employeesDB"
+
+});
+
+// connection.connect( (err) => {
+//   if (err) throw err;
+// });
+
+// connection.query = util.promisify(connection.query);
+
+module.exports = connection;
